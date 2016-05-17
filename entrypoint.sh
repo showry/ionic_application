f6d55aab082ce4207fd586c63ebf4f390631d389
showry <showry28@gmail.com>
2016-05-17 12:38:47 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/samhitha30/ionic-app.git /application
+git https://github.com/showry/ionic-app.git /application
 cd /application
 unzip sensor.zip
 cd /application/sensor
